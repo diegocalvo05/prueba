6bd714431389b2924b2a5bd9ae29f1e4ce836ecf
diegohdez33 <00007222@uca.edu.sv>
2022-11-09 17:29:35 -0600

    ✨ Add: Teams functions(list)

--- ProyectoFinal/ProyectoFinal/src/List.cpp
+++ ProyectoFinal/ProyectoFinal/src/List.cpp
@@ -1,1 +1,34 @@
 #include "List.h"
+
+void insertarLista(string pname) {
+    Pais* nuevoPais = new Pais();
+    nuevoPais->name = pname;
+
+    if (first == NULL) {
+        first = nuevoPais;
+        first->sig = first;
+        last = first;
+    }
+    else {
+        last->sig = nuevoPais;
+        nuevoPais->sig = first;
+        last = nuevoPais;
+    }
+}
+
+void imprimir() {
+    Pais* temp = first;
+
+    if (first != NULL) {
+        do {
+            cout << temp->name << endl;
+            temp = temp->sig;
+        } while (temp != first);
+    }
+}
+
+Pais* avanzar() {
+    Pais* temp = first;
+    temp = temp->sig;
+    return temp;
+}
--- ProyectoFinal/ProyectoFinal/src/List.h
+++ ProyectoFinal/ProyectoFinal/src/List.h
@@ -1,3 +1,16 @@
 #pragma once
+#include <iostream>
+#include <stdlib.h>
 
+using namespace std;
 
+//creeando la lista de los paises
+struct Pais {
+    string name;
+    int goles;
+    Pais* sig;
+}*first, * last;
+
+void insertarLista(string pname);
+void imprimir();
+Pais* avanzar();
